8cdc315b3ae5e7621054c95088ec25f61db5543c
Shubham Parihar <shubhamparihar391@gmail.com>
2024-09-08 14:05:47 +0530

    style: improve style of options page

--- src/Options.ts
+++ src/Options.ts
@@ -8,10 +8,10 @@ const saveOptions = () => {
 	const githubPAT = (document.getElementById('github_pat') as HTMLInputElement)!.value;
 	const githubRepo = (document.getElementById('github_repo') as HTMLInputElement)!.value;
 	chrome.storage.local.set({ githubPAT, githubRepo }, () => {
-		const status = document.getElementById('status')!;
-		status.textContent = 'Option Saved';
+		const status = document.getElementById('save_status')!;
+		status.style.display = 'block';
 		setTimeout(() => {
-			status.textContent = '';
+			status.style.display = 'none';
 		}, 1000);
 	});
 };
